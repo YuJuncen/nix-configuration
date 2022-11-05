e030d99d97386632215a7190d7155458e5f72a54
hillium <yu745514916@live.com>
2022-11-06 01:47:47 +0800

    using inherit
    Signed-off-by: hillium <yu745514916@live.com>

--- derivations/rofi-nord-theme/default.nix
+++ derivations/rofi-nord-theme/default.nix
@@ -1,10 +1,11 @@
 pkgs:
-
+with pkgs;
 derivation {
+    inherit busybox;
+
     name = "rofi-nord-theme";
     system = "x86_64-linux";
     src = ./nord.rasi;
-    busybox = pkgs.busybox;
     builder = "${pkgs.bash}/bin/bash";
     args = [ ./build.sh ];
 }
